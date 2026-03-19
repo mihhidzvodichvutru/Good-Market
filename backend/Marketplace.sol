0faec8f37f896cfb3727e408a8b13ef6178647ba
haicter <dhai29047@gmail.com>
2026-03-20 00:33:39 +0700

    Khởi tạo

--- backend/Marketplace.sol
+++ backend/Marketplace.sol
@@ -5,9 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract NFTMarketplace is ReentrancyGuard {
-    // Phí sàn (ví dụ 1%)
     uint256 public constant feePercent = 1;
-    address payable public immutable feeAccount; // Tài khoản nhận phí của team BODOI
+    address payable public immutable feeAccount; 
     uint256 public itemCount;
 
     struct Item {
@@ -19,7 +18,6 @@ contract NFTMarketplace is ReentrancyGuard {
         bool sold;
     }
 
-    // Lưu trữ danh sách các NFT đang bán trên sàn
     mapping(uint256 => Item) public items;
 
     event Offered(uint256 itemId, address indexed nft, uint256 tokenId, uint256 price, address indexed seller);
@@ -29,12 +27,10 @@ contract NFTMarketplace is ReentrancyGuard {
         feeAccount = payable(msg.sender);
     }
 
-    // Hàm 1: Đưa NFT lên sàn (Listing)
     function listNFT(IERC721 _nft, uint256 _tokenId, uint256 _price) external nonReentrant {
-        require(_price > 0, "Gia phai lon hon 0");
+        require(_price > 0, "Price must be greater than 0");
         itemCount++;
 
-        // Chuyển NFT từ ví người bán vào Smart Contract của sàn để giữ (Escrow)
         _nft.transferFrom(msg.sender, address(this), _tokenId);
 
         items[itemCount] = Item(itemCount, _nft, _tokenId, _price, payable(msg.sender), false);
@@ -42,24 +38,22 @@ contract NFTMarketplace is ReentrancyGuard {
         emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 
-    // Hàm 2: Mua NFT (Buying)
     function buyNFT(uint256 _itemId) external payable nonReentrant {
         uint256 _totalPrice = getTotalPrice(_itemId);
         Item storage item = items[_itemId];
         
-        require(_itemId > 0 && _itemId <= itemCount, "San pham khong ton tai");
-        require(msg.value >= _totalPrice, "Khong du tien eth de mua");
-        require(!item.sold, "San pham da duoc ban");
+        require(_itemId > 0 && _itemId <= itemCount, "Item doesn't exist");
+        require(msg.value >= _totalPrice, "Not enough ether to cover item price and market fee");
+        require(!item.sold, "Item already sold");
 
-        item.seller.transfer(item.price); // Trả tiền cho người bán
-        feeAccount.transfer(_totalPrice - item.price); // Trả phí sàn cho team
+        item.seller.transfer(item.price); 
+        feeAccount.transfer(_totalPrice - item.price); 
         item.sold = true;
-        item.nft.transferFrom(address(this), msg.sender, item.tokenId); // Chuyển NFT cho người mua
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId); 
 
         emit Bought(_itemId, address(item.nft), item.tokenId, item.price, item.seller, msg.sender);
     }
 
-    // Tính tổng tiền = Giá NFT + Phí sàn
     function getTotalPrice(uint256 _itemId) public view returns(uint256) {
         return (items[_itemId].price * (100 + feePercent)) / 100;
     }
--- backend/NFT.sol
+++ backend/NFT.sol
@@ -6,15 +6,13 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract BODOINFT is ERC721URIStorage {
     uint256 private _nextTokenId;
     
-    // Khởi tạo tên Token và Mã giao dịch cho sàn của team mình
     constructor() ERC721("BODOI NFT", "BFT") {}
 
-    // Hàm đúc NFT mới, nhận vào đường dẫn IPFS (chứa hình ảnh & metadata)
     function mint(string memory tokenURI) public returns (uint256) {
         uint256 tokenId = _nextTokenId++;
         
-        _mint(msg.sender, tokenId); // Đúc và giao quyền sở hữu cho người gọi hàm
-        _setTokenURI(tokenId, tokenURI); // Gắn link IPFS vào token
+        _mint(msg.sender, tokenId); 
+        _setTokenURI(tokenId, tokenURI); 
         
         return tokenId;
     }
